c0c62477fadd76243f798aff33249c77cf602f6c
Yao <mgzjys@gmail.com>
2020-03-19 17:39:44 -0400

    Update Timeline

---
+++ map/mapa_new.js
@@ -0,0 +1,438 @@
+/* Author(s) = Manuel Maroto (@manuelgmaroto), Luis Sevillano (@SepirData)
+ * Date: 2014 Oct
+ */
+var trimestres = new Array(
+  "2020-01-21",
+  "2020-01-22",
+  "2020-01-23",
+  "2020-01-24",
+  "2020-01-25",
+  "2020-01-26",
+  "2020-01-27",
+  "2020-01-28",
+  "2020-01-29",
+  "2020-01-30",
+  "2020-01-31",
+  "2020-02-01",
+  "2020-02-02",
+  "2020-02-03",
+  "2020-02-04",
+  "2020-02-05",
+  "2020-02-06",
+  "2020-02-07",
+  "2020-02-08",
+  "2020-02-09",
+  "2020-02-10",
+  "2020-02-11",
+  "2020-02-12",
+  "2020-02-13",
+  "2020-02-14",
+  "2020-02-15",
+  "2020-02-16",
+  "2020-02-17",
+  "2020-02-18",
+  "2020-02-19",
+  "2020-02-20",
+  "2020-02-21",
+  "2020-02-22",
+  "2020-02-23",
+  "2020-02-24",
+  "2020-02-25",
+  "2020-02-26",
+  "2020-02-27",
+  "2020-02-28",
+  "2020-02-29",
+  "2020-03-01",
+  "2020-03-02",
+  "2020-03-03",
+  "2020-03-04",
+  "2020-03-05",
+  "2020-03-06",
+  "2020-03-07",
+  "2020-03-08",
+  "2020-03-09",
+  "2020-03-10",
+  "2020-03-11",
+  "2020-03-12",
+  "2020-03-13",
+  "2020-03-14",
+  "2020-03-15",
+  "2020-03-16",
+  "2020-03-17"
+);
+
+function addComas(n) {
+  var formatValue = d3.format("0,000");
+  return formatValue(n)
+    .replace(".", ",")
+    .replace(".", ",");
+}
+var colores = new Array("#ECC5B4", "#E3A78F", "#D9886C", "#D0694C", "#A55036");
+
+function getColor(d) {
+  return d > 50
+    ? colores[4]
+    : d > 20
+    ? colores[3]
+    : d > 10
+    ? colores[2]
+    : d > 5
+    ? colores[1]
+    : colores[0];
+}
+
+var div = d3
+  .select("#wrapper")
+  .append("div")
+  .attr("class", "tooltip")
+  .attr("opacity", 0);
+
+var wmap = 1000;
+var hmap = 768;
+var hCan = 100;
+var wCan = 240;
+var projection = d3.geo
+  .mercator()
+  .translate([1520, 750])
+//  .center([38,-85])
+  .scale(700);
+var path = d3.geo.path().projection(projection);
+var map = d3
+  .select("#mapa")
+  .append("svg")
+  .attr("width", wmap)
+  .attr("height", hmap);
+//var projectionCan = d3.geo
+//  .mercator()
+//  .translate([810, 1350])
+//  .scale(2500);
+//var pathCan = d3.geo.path().projection(projectionCan);
+//var mapCan = d3
+//  .select("#canarias")
+//  .append("svg")
+//  .attr("width", wCan)
+//  .attr("height", hCan);
+
+d3.select("#year").html(trimestres[trimestres.length - 1].substring(0, 4));
+d3.select("#monthday").html(
+  trimestres[trimestres.length - 1].substring(5)
+);
+
+var height = 330,
+  width = 885,
+  trans = 60;
+var w = 950,
+  h = 380;
+var aux = trimestres.length - 1;
+var width_slider = 900;
+var height_slider = 50;
+d3.csv("../data/Without_Geo.csv", function(data) {
+  d3.json("../data/very_light.json", function(json) {
+      /* ------SLIDER----- */
+      var svg = d3
+        .select("#slider")
+        .attr("class", "chart")
+        .append("svg")
+        .attr("width", width_slider)
+        .attr("height", height_slider);
+      var yeardomain = [0, trimestres.length - 1];
+      var axisyears = [
+        parseFloat(trimestres[0].substring(6)),
+        parseFloat(trimestres[trimestres.length - 1].substring(6))
+      ];
+
+      var pointerdata = [
+        {
+          x: 0,
+          y: 0
+        },
+        {
+          x: 0,
+          y: 25
+        },
+        {
+          x: 25,
+          y: 25
+        },
+        {
+          x: 25,
+          y: 0
+        }
+      ];
+      var scale = d3.scale
+        .linear()
+        .domain(yeardomain)
+        .rangeRound([0, width]);
+      var x = d3.svg
+        .axis()
+        .scale(scale)
+        .orient("top")
+        .tickFormat(function(d) {
+          return d;
+        })
+        .tickSize(0)
+        .tickValues(axisyears);
+      svg
+        .append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(" + 15 + ",0)")
+        .call(x);
+      var drag = d3.behavior
+        .drag()
+        .origin(function() {
+          return {
+            x: d3.select(this).attr("x"),
+            y: d3.select(this).attr("y")
+          };
+        })
+        .on("dragstart", dragstart)
+        .on("drag", dragmove)
+        .on("dragend", dragend);
+
+      svg
+        .append("g")
+        .append("rect")
+        .attr("class", "slideraxis")
+        .attr("width", width_slider)
+        .attr("height", 7)
+        .attr("x", 0)
+        .attr("y", 16);
+      var cursor = svg
+        .append("g")
+        .attr("class", "move")
+        .append("svg")
+        .attr("x", width)
+        .attr("y", 7)
+        .attr("width", 30)
+        .attr("height", 60);
+
+      cursor.call(drag);
+      var drawline = d3.svg
+        .line()
+        .x(function(d) {
+          return d.x;
+        })
+        .y(function(d) {
+          return d.y;
+        })
+        .interpolate("linear");
+
+      //---------------------------
+      cursor
+        .append("path")
+        .attr("class", "cursor")
+        .attr("transform", "translate(" + 7 + ",0)")
+        .attr("d", drawline(pointerdata));
+      cursor.on("mouseover", function() {
+        d3.select(".move").style("cursor", "hand");
+      });
+
+      function dragmove() {
+        var x = Math.max(0, Math.min(width, d3.event.x));
+        d3.select(this).attr("x", x);
+        var z = parseInt(scale.invert(x));
+        aux = z;
+        drawMap(z);
+      }
+
+      function dragstart() {
+        d3.select(".cursor").style("fill", "#D9886C");
+      }
+
+      function dragend() {
+        d3.select(".cursor").style("fill", "");
+      }
+      for (var i = 0; i < data.length; i++) {
+        var codeState = data[i].fips;
+        var dataValue = data[i][trimestres[trimestres.length - 1]];
+        //                console.log(dataValue);
+        for (var j = 0; j < json.features.length; j++) {
+          var jsonState = json.features[j].properties.fips;
+          if (codeState == jsonState) {
+            json.features[j].properties.value = dataValue;
+            break;
+          }
+        }
+      }
+      var cont = map
+        .selectAll("#mapa path")
+        .data(json.features)
+        .enter()
+        .append("path")
+        .attr("class", "path")
+        .attr("d", path)
+        .style("fill", function(d) {
+          return getColor(d.properties.value);
+        })
+        .attr("fill-opacity", "1")
+        .attr("stroke", "#202020")
+        .attr("stroke-width", 0.3)
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseout", mouseout);
+
+
+      function mouseover(d) {
+        d3.select(this)
+          .attr("stroke-width", "1px")
+          .attr("fill-opacity", "0.9");
+        div.style("opacity", 0.9);
+        div.html(
+          "<b>" +
+            d.properties.NAME +
+            "</b></br>Tasa paro: <b>" +
+            addComas(data[d.properties.fips][trimestres[aux]]) +
+            "%</b> <br>" +
+            d.properties.NAME
+        );
+      }
+
+      function mouseout(d) {
+        d3.select(this)
+          .attr("stroke-width", ".3")
+          .attr("fill-opacity", "1");
+        div.style("opacity", 0);
+      }
+
+      function mousemove(d) {
+        div.style({
+          left: function() {
+            if (d3.event.pageX > 780) {
+              return d3.event.pageX - 180 + "px";
+            } else {
+              return d3.event.pageX + 23 + "px";
+            }
+          },
+          top: d3.event.pageY - 20 + "px"
+        });
+      }
+      //maxMin(data, aux);
+      function drawMap(index) {
+        d3.select("#monthday").html(trimestres[index].substring(5));
+        d3.select("#year").html(trimestres[index].substring(0, 4));
+        cont.style("fill", function(d) {
+          for (var i = 0; i < data.length; i++) {
+            var codeState = data[i].fips;
+            var dataValue = data[i][trimestres[index]];
+            for (var j = 0; j < json.features.length; j++) {
+              var jsonState = json.features[j].properties.fips;
+              if (codeState == jsonState) {
+                json.features[j].properties.value = dataValue;
+                break;
+              }
+            }
+          }
+          var value = d.properties.value;
+          if (value) {
+            return getColor(value);
+          } else {
+            return "#ccc";
+          }
+        });
+        cont
+          .on("mousemove", function(d) {
+            div.style("opacity", 0.9);
+            div
+              .html(
+                "<b>" +
+                  d.properties.NAME +
+                  "</b></br>Tasa de paro: <b>" +
+                  addComas(data[d.properties.fips][trimestres[aux]]) +
+                  "%</b> <br>" +
+                  d.properties.NAME
+              )
+              .style("left", function() {
+                if (d3.event.pageX > 780) {
+                  return d3.event.pageX - 180 + "px";
+                } else {
+                  return d3.event.pageX + 23 + "px";
+                }
+              })
+              .style("top", d3.event.pageY - 20 + "px");
+          })
+          .on("mouseout", function() {
+            return div.style("opacity", 0);
+          })
+          .on("mouseout", mouseout)
+          .on("mousemove", function(d) {
+            div.style("opacity", 0.9);
+            div
+              .html(
+                "<b>" +
+                  d.properties.NAME +
+                  "</b></br>Tasa de paro: <b>" +
+                  addComas(data[d.properties.fips][trimestres[aux]]) +
+                  "%</b> <br>" +
+                  d.properties.NAME
+              )
+              .style("left", function() {
+                if (d3.event.pageX > 780) {
+                  return d3.event.pageX - 180 + "px";
+                } else {
+                  return d3.event.pageX + 23 + "px";
+                }
+              })
+              .style("top", d3.event.pageY - 20 + "px");
+          })
+          .on("mouseout", function() {
+            return div.style("opacity", 0);
+          })
+          .on("mouseout", mouseout);
+        maxMin(data, index);
+      }
+      maxMin(data, aux);
+
+      function maxMin(d, index) {
+        d3.select("#minimoParo").html("");
+        d3.select("#maximoParo").html("");
+        d3.select("#mediaParo").html("");
+        var datos = [];
+        var provincia = [];
+        for (var i = 0; i < data.length - 1; i++) {
+          //-1 para que no cargue Nacional
+          datos.push(d[i][trimestres[index]]);
+          provincia.push(d[i].NAME);
+        }
+        var max_min = d3.extent(datos);
+        console.log(max_min);
+        var provinciaMax;
+        var provinciaMin;
+        for (var j = 0; j < data.length - 1; j++) {
+          if (max_min[0] == datos[j]) {
+            provinciaMin = provincia[j];
+          }
+          if (max_min[1] == datos[j]) {
+            provinciaMax = provincia[j];
+          }
+        }
+        var nombreMediaParo = d3
+          .select("#mediaParo")
+          .html(addComas(data[52][trimestres[index]]) + "%");
+        var nombreProvinciaMax = d3
+          .select("#maximoParo")
+          .html(
+            addComas(max_min[1]) +
+              "%<br>" +
+              "<span id='provincia'>" +
+              provinciaMax +
+              "</span>"
+          );
+        var nombreProvinciaMin = d3
+          .select("#minimoParo")
+          .html(
+            addComas(max_min[0]) +
+              "%<br>" +
+              "<span id='provincia'>" +
+              provinciaMin +
+              "</span"
+          );
+      };
+  });
+});
+
+d3.select("#wrapper").on("touchstart", function() {
+  div
+    .transition()
+    .duration(100)
+    .style("opacity", 0);
+});
---
+++ map/mapa_simplyfied.js
@@ -0,0 +1,443 @@
+/* Author(s) = Yao Li (mgzjys@gmail.com)
+ * Date: 2020 March
+ */
+var timearry = new Array(
+  "2020-01-21",
+  "2020-01-22",
+  "2020-01-23",
+  "2020-01-24",
+  "2020-01-25",
+  "2020-01-26",
+  "2020-01-27",
+  "2020-01-28",
+  "2020-01-29",
+  "2020-01-30",
+  "2020-01-31",
+  "2020-02-01",
+  "2020-02-02",
+  "2020-02-03",
+  "2020-02-04",
+  "2020-02-05",
+  "2020-02-06",
+  "2020-02-07",
+  "2020-02-08",
+  "2020-02-09",
+  "2020-02-10",
+  "2020-02-11",
+  "2020-02-12",
+  "2020-02-13",
+  "2020-02-14",
+  "2020-02-15",
+  "2020-02-16",
+  "2020-02-17",
+  "2020-02-18",
+  "2020-02-19",
+  "2020-02-20",
+  "2020-02-21",
+  "2020-02-22",
+  "2020-02-23",
+  "2020-02-24",
+  "2020-02-25",
+  "2020-02-26",
+  "2020-02-27",
+  "2020-02-28",
+  "2020-02-29",
+  "2020-03-01",
+  "2020-03-02",
+  "2020-03-03",
+  "2020-03-04",
+  "2020-03-05",
+  "2020-03-06",
+  "2020-03-07",
+  "2020-03-08",
+  "2020-03-09",
+  "2020-03-10",
+  "2020-03-11",
+  "2020-03-12",
+  "2020-03-13",
+  "2020-03-14",
+  "2020-03-15",
+  "2020-03-16",
+  "2020-03-17"
+);
+
+function addComas(n) {
+  var formatValue = d3.format("0,000");
+  return formatValue(n)
+    .replace(".", ",")
+    .replace(".", ",");
+}
+
+
+var colores = d3.schemeReds[5]
+
+function getColor(d) {
+  return d > 100
+    ? colores[4]
+    : d > 50
+    ? colores[3]
+    : d > 10
+    ? colores[2]
+    : d > 0
+    ? colores[1]
+    : colores[0];
+}
+
+var div = d3
+  .select("#wrapper")
+  .append("div")
+  .attr("class", "tooltip")
+  .attr("opacity", 0);
+
+var wmap = 900;
+var hmap = 500;
+var projection = d3.geoAlbersUsa()
+     .scale(900)
+     .translate([480, 270]);
+var path = d3.geo.path().projection(projection);
+
+var map = d3
+  .select("#mapa")
+  .append("svg")
+  .attr("width", wmap)
+  .attr("height", hmap);
+//var projectionCan = d3.geo
+//  .mercator()
+//  .translate([810, 1350])
+//  .scale(2500);
+//var pathCan = d3.geo.path().projection(projectionCan);
+//var mapCan = d3
+//  .select("#canarias")
+//  .append("svg")
+//  .attr("width", wCan)
+//  .attr("height", hCan);
+d3.select("#maptitle").html("US Covid-19 Cases");
+
+d3.select("#year").html(timearry[timearry.length - 1].substring(0, 4));
+d3.select("#monthday").html(timearry[timearry.length - 1].substring(5));
+
+var height = 330,
+  width = 885,
+  trans = 60;
+var w = 950,
+  h = 380;
+var aux = timearry.length - 1;
+var width_slider = 950;
+var height_slider = 50;
+d3.csv("../data/maybe.csv", function(data) {
+  d3.json("../data/maybe_geo.json", function(json) {
+      /* ------SLIDER----- */
+      var svg = d3
+        .select("#slider")
+        .attr("class", "chart")
+        .append("svg")
+        .attr("width", width_slider)
+        .attr("height", height_slider);
+      var yeardomain = [0, timearry.length - 1];
+      var axisyears = [
+//        parseFloat(timearry[0].substring(6)),
+//        parseFloat(timearry[timearry.length - 1].substring(6))
+1,57
+      ];
+
+      var pointerdata = [
+        {
+          x: 0,
+          y: 0
+        },
+        {
+          x: 0,
+          y: 25
+        },
+        {
+          x: 25,
+          y: 25
+        },
+        {
+          x: 25,
+          y: 0
+        }
+      ];
+      var scale = d3.scale
+        .linear()
+        .domain(yeardomain)
+        .rangeRound([0, width]);
+      var x = d3.svg
+        .axis()
+        .scale(scale)
+        .orient("top")
+        .tickFormat(function(d) {
+          return d;
+        })
+        .tickSize(0)
+        .tickValues(axisyears);
+      svg
+        .append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(" + 15 + ",0)")
+        .call(x);
+      var drag = d3.behavior
+        .drag()
+        .origin(function() {
+          return {
+            x: d3.select(this).attr("x"),
+            y: d3.select(this).attr("y")
+          };
+        })
+        .on("dragstart", dragstart)
+        .on("drag", dragmove)
+        .on("dragend", dragend);
+
+      svg
+        .append("g")
+        .append("rect")
+        .attr("class", "slideraxis")
+        .attr("width", width_slider)
+        .attr("height", 7)
+        .attr("x", 0)
+        .attr("y", 16);
+      var cursor = svg
+        .append("g")
+        .attr("class", "move")
+        .append("svg")
+        .attr("x", 880)
+        .attr("y", 7)
+        .attr("width", 15)
+        .attr("height", 60);
+
+      cursor.call(drag);
+      var drawline = d3.svg
+        .line()
+        .x(function(d) {
+          return d.x;
+        })
+        .y(function(d) {
+          return d.y;
+        })
+        .interpolate("linear");
+
+      //---------------------------
+      cursor
+        .append("path")
+        .attr("class", "cursor")
+        .attr("transform", "translate(" + 7 + ",0)")
+        .attr("d", drawline(pointerdata));
+      cursor.on("mouseover", function() {
+        d3.select(".move").style("cursor", "hand");
+      });
+
+      function dragmove() {
+        var x = Math.max(0, Math.min(width, d3.event.x));
+        d3.select(this).attr("x", x);
+        var z = parseInt(scale.invert(x));
+        aux = z;
+        drawMap(z);
+      }
+
+      function dragstart() {
+        d3.select(".cursor").style("fill", "#D9886C");
+      }
+
+      function dragend() {
+        d3.select(".cursor").style("fill", "");
+      }
+      for (var i = 0; i < data.length; i++)
+      {
+        var codeState = data[i].ID;
+        var dataValue = parseFloat(data[i][timearry[timearry.length - 1]]);
+        console.log(dataValue);
+        for (var j = 0; j < json.features.length; j++)
+        {
+          var jsonState = json.features[j].properties.ID;
+          if (codeState == jsonState) {
+            json.features[j].properties.value = dataValue;
+            break;
+          }
+        }
+      }
+      var cont = map
+        .selectAll("#mapa path")
+        .data(json.features)
+        .enter()
+        .append("path")
+        .attr("class", "path")
+        .attr("d", path)
+        .style("fill", function(d) {
+          return getColor(d.properties.value);
+        })
+        .attr("fill-opacity", "1")
+        .attr("stroke", "#202020")
+        .attr("stroke-width", 0.3)
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseout", mouseout);
+
+
+      function mouseover(d) {
+        d3.select(this)
+          .attr("stroke-width", "1px")
+          .attr("fill-opacity", "0.9");
+        div.style("opacity", 0.9);
+        div.html(
+          "<b>" +
+            d.properties.NAME +
+            "</b></br>Positve cases: <b>" +
+            addComas(data[d.properties.ID][timearry[aux]]) +
+            "</b></br>FIPS:" +
+            d.properties.fip
+        );
+      }
+
+      function mouseout(d) {
+        d3.select(this)
+          .attr("stroke-width", ".3")
+          .attr("fill-opacity", "1");
+        div.style("opacity", 0);
+      }
+
+      function mousemove(d) {
+        div.style({
+          left: function() {
+            if (d3.event.pageX > 780) {
+              return d3.event.pageX - 180 + "px";
+            } else {
+              return d3.event.pageX + 23 + "px";
+            }
+          },
+          top: d3.event.pageY - 20 + "px"
+        });
+      }
+      //maxSum(data, aux);
+      function drawMap(index) {
+        d3.select("#monthday").html(timearry[index].substring(5));
+        d3.select("#year").html(timearry[index].substring(0, 4));
+        cont.style("fill", function(d) {
+          for (var i = 0; i < data.length; i++)
+          {
+            var codeState = data[i].ID;
+            var dataValue = data[i][timearry[index]];
+            json.features[i].properties.value = dataValue;
+
+          }
+          var value = d.properties.value;
+          if (value) {
+            return getColor(value);
+          } else {
+            return "#ccc";
+          }
+        });
+        cont
+          .on("mousemove", function(d) {
+            div.style("opacity", 0.9);
+            div
+              .html(
+                "<b>" +
+                  d.properties.NAME +
+                  "</b></br>Positve cases: <b>" +
+                  addComas(data[d.properties.ID][timearry[aux]]) +
+                  "</b></br>FIPS:" +
+                  d.properties.fip
+              )
+              .style("left", function() {
+                if (d3.event.pageX > 780) {
+                  return d3.event.pageX - 180 + "px";
+                } else {
+                  return d3.event.pageX + 23 + "px";
+                }
+              })
+              .style("top", d3.event.pageY - 20 + "px");
+          })
+          .on("mouseout", function() {
+            return div.style("opacity", 0);
+          })
+          .on("mouseout", mouseout)
+          .on("mousemove", function(d) {
+            div.style("opacity", 0.9);
+            div
+              .html(
+                "<b>" +
+                  d.properties.NAME +
+                  "</b></br>Postive cases: <b>" +
+                  addComas(data[d.properties.ID][timearry[aux]]) +
+                  "</b></br>FIPS:" +
+                  d.properties.fip
+              )
+              .style("left", function() {
+                if (d3.event.pageX > 780) {
+                  return d3.event.pageX - 180 + "px";
+                } else {
+                  return d3.event.pageX + 23 + "px";
+                }
+              })
+              .style("top", d3.event.pageY - 20 + "px");
+          })
+          .on("mouseout", function() {
+            return div.style("opacity", 0);
+          })
+          .on("mouseout", mouseout);
+        maxSum(data, index);
+      }
+     maxSum(data, aux);
+
+      function maxSum(d, index) {
+        d3.select("#posicountynum").html("");
+        d3.select("#maxcountynum").html("");
+        d3.select("#totalcasenumber").html("");
+        var datos = [];
+        var county = [];
+        for (var i = 0; i < data.length; i++) {
+          datos.push(parseFloat(d[i][timearry[index]]));
+          county.push(d[i].NAME);
+        }
+        var max_sum = d3.extent(datos);
+        console.log(max_sum);
+        var countyMax;
+        var countyPoNum;
+        var tatalcaseNum =0;
+        var posicounty =0;
+        for (var j = 0; j < data.length; j++)
+        {
+          if (parseFloat(datos[j]) > 0)
+          {
+            posicounty=posicounty+1;
+          }
+          if (max_sum[1] == parseFloat(datos[j]))
+          {
+            countyMax = county[j];
+          }
+          tatalcaseNum = tatalcaseNum+parseFloat(datos[j]);
+        }
+        max_sum[0]=posicounty;
+
+        countyPoNum = [' counties have positve cases'];
+        var nombretotalcasenumber = d3
+          .select("#totalcasenumber")
+          .html(addComas(tatalcaseNum));
+        var nombrecountyMax = d3
+          .select("#maxcountynum")
+          .html(
+          //  addComas(max_sum[1]) +
+              "<br>" +
+              "<span id='county'>" +
+              countyMax + "  has the most postive cases, which is  " + max_sum[1]+
+              "</span>"
+          );
+        var nombrecountyPoNum = d3
+          .select("#posicountynum")
+          .html(
+        //    addComas(max_sum[0]) +
+              "<br>" +
+              "<span id='county'>" + max_sum[0]+
+              countyPoNum +
+              "</span"
+          );
+      };
+  });
+});
+
+d3.select("#wrapper").on("touchstart", function() {
+  div
+    .transition()
+    .duration(100)
+    .style("opacity", 0);
+});
