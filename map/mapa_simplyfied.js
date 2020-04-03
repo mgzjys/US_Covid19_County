e114b0a44db4ea4968ed7a6b004cab2a5e89137e
Yao <mgzjys@gmail.com>
2020-04-03 17:58:12 -0400

    update

--- map/mapa_simplyfied.js
+++ map/mapa_simplyfied.js
@@ -8,7 +8,7 @@ d3 = d3versionV3;
 
 var jsonOutside;
 var active;
-var unassigned = 8246;/////
+var unassigned = 8482;/////
 var scalefactor = 100000;
 var height = 330,
 width = 1180,
@@ -155,7 +155,7 @@ d3.select("#mapsubtitle").html("(one-click on map to zoom in; double-click to zo
 
 d3.select("#creditinfor").html("Created by GISers from CGIS, UMD");
 
-d3.select("#datainfor").html("Data updated time (09:00,14:00,18:00,22:00) : 2020-04-03 14:00 EST");
+d3.select("#datainfor").html("Data updated time (09:00,14:00,18:00,22:00) : 2020-04-03 18:00 EST");
 
 d3.select("#contributions").html("Contribution: Visualization by Yao Li and Zheng Liu. Data collection by Junchuan Fan, Hai Lan, Yao Li, Jeff Sauer, Zhiyue Xia,Guiming Zhu from CGIS, University of Maryland, College Park.");
 
