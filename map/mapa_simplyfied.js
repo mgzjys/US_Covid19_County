ea5392c8e1c081c2754076ac42aa41cb6af945ef
Yao <mgzjys@gmail.com>
2020-04-21 18:42:52 -0400

    update

--- map/mapa_simplyfied.js
+++ map/mapa_simplyfied.js
@@ -12,7 +12,7 @@ d3 = d3versionV3;
 var selectID;
 var jsonOutside;
 var active;
-var unassigned =  19808;/////
+var unassigned =  22169;/////
 var scalefactor = 100000;
 var height = 330,
 width = 1180,
